b054a3ddc544014ff184dcf584311fa6ae8ceab0
GCBallesteros <dev+github@maxwellrules.com>
2025-01-20 20:51:50 +0100

    feat: Move texture assets to S3 and LODS

--- src/earth.ts
+++ src/earth.ts
@@ -1,7 +1,13 @@
 import * as THREE from 'three';
 
-import earthTextureUrl from '../earth_texture.jpg';
 import { createFrame } from './components.js';
+
+const lowResEarthTextureUrl =
+  'https://whatoneaerth.s3.eu-west-1.amazonaws.com/earth_texture_LR.jpg';
+const midResEarthTextureUrl =
+  'https://whatoneaerth.s3.eu-west-1.amazonaws.com/earth_texture_MR.jpg';
+const highResEarthTextureUrl =
+  'https://whatoneaerth.s3.eu-west-1.amazonaws.com/earth_texture_HR.jpg';
 
 const RADIUS_EARTH = 6371.0;
 
@@ -11,22 +17,43 @@ export function makeEarth(): { earth: THREE.Mesh; earth_frame: THREE.Group } {
   const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 
   const textureLoader = new THREE.TextureLoader();
-  textureLoader.load(
-    earthTextureUrl,
-    (texture) => {
-      earth.material.map = texture;
-      earth.material.needsUpdate = true;
-      console.log('Earth texture loaded');
-    },
-    undefined,
-    (error) => {
-      console.error('Error loading texture:', error);
-    },
-  );
+
+  const textureLevels = [
+    lowResEarthTextureUrl,
+    midResEarthTextureUrl,
+    highResEarthTextureUrl,
+  ];
+
+  let currentLevel = 0;
+
+  const loadNextTexture = () => {
+    if (currentLevel >= textureLevels.length) return;
+
+    const textureUrl = textureLevels[currentLevel];
+    textureLoader.load(
+      textureUrl,
+      (texture) => {
+        earth.material.map = texture;
+        earth.material.needsUpdate = true;
+        console.log(`Texture level ${currentLevel + 1} loaded`);
+        currentLevel++;
+
+        // Load the next level after a delay to avoid blocking
+        setTimeout(loadNextTexture, 2000); // Adjust delay as needed
+      },
+      undefined,
+      (error) => {
+        console.error(`Error loading texture at level ${currentLevel}:`, error);
+      },
+    );
+  };
+
+  loadNextTexture();
 
   earth.rotation.x = Math.PI / 2;
 
-  let earth_frame = createFrame(
+  // Create the earth frame
+  const earth_frame = createFrame(
     {
       x: 0,
       y: 0,
@@ -34,6 +61,7 @@ export function makeEarth(): { earth: THREE.Mesh; earth_frame: THREE.Group } {
     },
     8000,
   );
+
   return {
     earth: earth,
     earth_frame: earth_frame,
