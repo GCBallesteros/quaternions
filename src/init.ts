63e1f5a26e1d5bedb36d5356decb421b8759d52e
GCBallesteros <dev+github@maxwellrules.com>
2025-01-24 14:32:43 +0100

    feat: Make sun less bright

--- src/init.ts
+++ src/init.ts
@@ -47,7 +47,7 @@ export function initScene(
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.2); 
   scene.add(ambientLight);
 
-  const sunLight = new THREE.DirectionalLight(0xffffff, 9.0);
+  const sunLight = new THREE.DirectionalLight(0xffffff, 7.0);
   scene.add(sunLight);
   
   updateSunLight(sunLight, new Date());
