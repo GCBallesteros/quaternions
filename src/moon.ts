89b8415b8d84766be1a57897c12878fffe8ff026
GCBallesteros <dev+github@maxwellrules.com>
2025-02-06 21:17:27 +0100

    refactor: Cleanup Moon rotation

--- src/moon.ts
+++ src/moon.ts
@@ -2,12 +2,6 @@ import * as THREE from 'three';
 
 const MOON_RADIUS = 1737.1;
 
-const MOON_ROTATION = {
-  x: Math.PI / 2,
-  y: Math.PI / 4,
-  z: 0,
-};
-
 export function makeMoon(): THREE.Mesh {
   const moonGeometry = new THREE.SphereGeometry(MOON_RADIUS, 64, 64);
   const textureLoader = new THREE.TextureLoader();
@@ -24,7 +18,7 @@ export function makeMoon(): THREE.Mesh {
   });
 
   const moon = new THREE.Mesh(moonGeometry, moonMaterial);
-  moon.rotation.set(MOON_ROTATION.x, MOON_ROTATION.y, MOON_ROTATION.z);
+  moon.rotation.x = Math.PI / 2;
 
   return moon;
 }
