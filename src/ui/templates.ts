da9a8250498380d42eaa9d57fc46d0b78bfd530d
GCBallesteros <dev+github@maxwellrules.com>
2025-02-16 11:06:33 +0100

    refactor: Improve plot download functionality using lit-html event handling

--- src/ui/templates.ts
+++ src/ui/templates.ts
@@ -105,10 +105,12 @@ const cameraDetailsTemplate = (point: OrientedPoint) => html`
   </div>
 `;
 
-export const plotTemplate = (plotId: string) => html`
+export const plotTemplate = (plotId: string, onDownload: () => void) => html`
   <div class="plot-header">
     <div class="plot-id">ID: ${plotId}</div>
-    <button class="plot-download-button">Download Data</button>
+    <button class="plot-download-button" @click=${onDownload}>
+      Download Data
+    </button>
   </div>
   <canvas width="800" height="400"></canvas>
 `;
