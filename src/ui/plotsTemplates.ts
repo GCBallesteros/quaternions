e6c4ae65bc787566e8de7bdfa07c5923b2d5955c
GCBallesteros <dev+github@maxwellrules.com>
2025-03-04 22:20:09 +0100

    fix: Plot styling
    Canvas was outside of card

--- src/ui/plotsTemplates.ts
+++ src/ui/plotsTemplates.ts
@@ -15,12 +15,16 @@ export const plotTemplate = (
   onDownload: () => void,
 ): TemplateResult<1> => html`
   <div class=${plotStyles.container}>
-    <div class=${plotStyles.header.wrapper}>
-      <h3 class=${commonStyles.sectionTitle}>ID: ${plotId}</h3>
-      <button class=${plotStyles.header.button} @click=${onDownload}>
-        Download Data
-      </button>
+    <div>
+      <div class=${plotStyles.header.wrapper}>
+        <h3 class=${plotStyles.header.id}>id: ${plotId}</h3>
+        <button class=${plotStyles.header.button} @click=${onDownload}>
+          Download Data
+        </button>
+      </div>
+      <div>
+        <canvas class=${plotStyles.canvas} width="800" height="400"></canvas>
+      </div>
     </div>
-    <canvas class=${plotStyles.canvas} width="800" height="400"></canvas>
   </div>
 `;
--- src/ui/styles/plots.ts
+++ src/ui/styles/plots.ts
@@ -1,13 +1,13 @@
 import { commonStyles } from './common.js';
 
 export const plotStyles = {
-  container: `${commonStyles.card} ${commonStyles.sectionContainer} relative w-full aspect-[2/1]`,
-  canvas: 'absolute inset-0 mt-10 mx-2 mb-2 object-contain',
+  container: `p-2 my-1 bg-neutral-800 rounded ${commonStyles.sectionContainer} relative w-full aspect-[2/1]`,
+  canvas: 'object-contain', // 'absolute inset-0 mt-10 mx-2 mb-2 object-contain',
   header: {
     wrapper:
-      'flex justify-between items-center p-1 px-2 bg-neutral-900 absolute top-0 left-0 right-0 z-10',
+      'flex justify-between items-center p-1 px-2 bg-neutral-900 relative -top-2 left-0 right-0 z-10 rounded-t-sm -mx-2',
     id: `${commonStyles.monospace} text-white text-sm`,
     button:
-      'bg-blue-600 hover:bg-blue-700 text-white border-none px-2 py-1 rounded text-xs cursor-pointer',
+      'bg-blue-600 hover:bg-blue-700 text-white border-none px-2 py-1 rounded-sm text-xs cursor-pointer',
   },
 };
